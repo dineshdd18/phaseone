8c97f9ac239cc6c6a73a7f6b49b01c9c6f696107
dineshdd18 <98386825+dineshdd18@users.noreply.github.com>
2022-02-09 10:43:11 +0530

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,36 @@
+package diamondproblem;
+
+ public class TestClass {
+	interface First 
+	{  
+	    default void show() 
+	    { 
+	        System.out.println("Default First"); 
+	    } 
+	} 
+	interface Second 
+	{  
+	    default void show() 
+	    { 
+	        System.out.println("Default Second"); 
+	    } 
+	}  
+	public class Testclass implements First, Second 
+	{  
+	    public void show() 
+	    {  
+	        First.super.show(); 
+	        Second.super.show(); 
+	    } 
+
+
+	public static void main(String[] args) {
+        TestClass ob = new TestClass(); 
+         ob.show(); 
+	}
+	}
+	public void show() {
+		System.out.println("the default value are");
+		
+	}
+ }
